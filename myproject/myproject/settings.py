faf043bddca4dd3372a501ac7eddee4fd8010407
sgrote14 <54189181+sgrote14@users.noreply.github.com>
2024-03-27 13:58:20 -0400

    configured for postgres db

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Sgk62114!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
